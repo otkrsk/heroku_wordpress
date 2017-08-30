38be16c240271240e66a9b7b4bc04b17f51effea
Nicholas Nuing <nicholas.nuing@gmail.com>
2017-08-30 11:06:44 +0800

    Removed the SSL configuration from the wp-config file.

--- wp-config.php
+++ wp-config.php
@@ -96,11 +96,3 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-
-/** SSL */
-define('FORCE_SSL_ADMIN', true);
-// in some setups HTTP_X_FORWARDED_PROTO might contain
-// a comma-separated list e.g. http,https
-// so check for https existence
-if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
-	$_SERVER['HTTPS']='on';
